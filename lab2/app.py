6ae55d0fe88f9a5804d87fa242b6fa959a5194c7
Diachkova Angelina <507543@niuitmo.ru>
2026-09-14 13:13:16 +0300

    Update app.py

--- lab2/app.py
+++ lab2/app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello from Flask!"
+    return "Hello from Docker!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
